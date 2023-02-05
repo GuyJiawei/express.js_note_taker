2cb161bedc95f2305adeb2d8630280d8fcb5a502
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-02-05 13:03:42 +1100

    added reamining notes

---
+++ Develop/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// Import the notes router
+const notesRouter = require('./notes');
+
+// Create an Express app instance
+const app = express();
+
+// Mount the notes router at the /notes endpoint
+app.use('./notes', notesRouter);
+
+//export the Express app instance
+module.exports = app;
