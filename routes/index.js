76916c1d466433cf63ecbbec82e6349eb08a773f
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-02-07 08:39:48 +1100

    took out unnecessary comments

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,6 @@ const app = require('express').Router();
 // Import the notes router
 const notesRouter = require('./notes');
 
-// Create an Express app instance
-// const app = express();
-
 // Mount the notes router at the /notes endpoint
 app.use('/notes', notesRouter);
 
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const path = require('path');
 
 // Import the API routes
 const api = require('./routes/index');
